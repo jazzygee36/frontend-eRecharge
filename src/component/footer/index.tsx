08bb61381f5b74d1f9b7e942d2af16696a08d745
Samson Mesioye <jazzygee36gmail.com>
2024-11-06 15:08:23 +0100

    ui enhancement

--- src/component/footer/index.tsx
+++ src/component/footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='py-5 bg-[green] m-auto'>
       <p className='text-white text-center'>
-        Copyright jazzygee36@gmail.com 2022{' '}
+        Copyright jazzygee36@gmail.com 2024{' '}
       </p>
     </div>
   );
